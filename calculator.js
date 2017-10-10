51c6d274a1eb2a9607243b4295400a06056eca4d
Luis Carlin <luiscarlin@gmail.com>
2017-10-10 07:02:54 -0400

    Modified files names

---
+++ calculator.js
@@ -0,0 +1,3 @@
+exports.add = (input1, input2) => {
+  return 2
+}
---
+++ spec/calculator-spec.js
@@ -0,0 +1,11 @@
+var chai = require('chai')
+var expect = chai.expect
+
+var underTest = require('../calculator')
+
+describe('#calculator', () => {
+  it('should add 1 and 1 and return 2', () => {
+    var result = underTest.add(1, 1)
+    expect(result).to.equal(2)
+  })
+})
