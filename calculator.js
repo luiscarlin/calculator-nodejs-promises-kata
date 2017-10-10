29cf4b7f798d0a036e415e8753107580553aacea
Luis Carlin <luiscarlin@gmail.com>
2017-10-10 07:34:11 -0400

    Rewrite with promises

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
 exports.add = (input1, input2) => {
-  return input1 + input2
+  return addPromise(input1, input2).then((resultPromise) => {
+    return resultPromise
+  })
+}
+
+addPromise = (input1, input2) => {
+  return new Promise((resolve, reject) => {
+    let result = input1 + input2
+    return resolve(result)
+  })
 }
--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -4,12 +4,12 @@ var expect = chai.expect
 var underTest = require('../calculator')
 
 describe('#calculator', () => {
-  it('should add 1 and 1 and return 2', () => {
-    var result = underTest.add(1, 1)
+  it('should add 1 and 1 and return 2', async () => {
+    var result = await underTest.add(1, 1)
     expect(result).to.equal(2)
   })
-  it('should add 1 and 4 and return 5', () => {
-    var result = underTest.add(1, 4)
+  it('should add 1 and 4 and return 5', async () => {
+    var result = await underTest.add(1, 4)
     expect(result).to.equal(5)
   })
 })
